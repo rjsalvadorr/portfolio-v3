296ef5fd681cc2d367e60c9a4141fbddd36e39ad
RJ Salvador <rjsalvadorr@gmail.com>
2019-05-14 22:18:37 -0400

    #7 - Added better colours to the box grid

--- .vuepress/components/ThreeCanvas.vue
+++ .vuepress/components/ThreeCanvas.vue
@@ -5,6 +5,9 @@
 
 <script>
 import * as THREE from 'three';
+import sample from "lodash/sample";
+import chroma from 'chroma-js';
+import utils from '../utils/three-utils';
 
 export default {
   name: 'ThreeCanvas',
@@ -12,15 +15,14 @@ export default {
     ///////////////////////////////////////////////////////////////////////////////
     //   CONSTANTS
 
-    const LIGHT_POS = new THREE.Vector3(2, 3, 1);
-    const CAMERA_POS = new THREE.Vector3(25, 15, 30);
-    const CAMERA_TARGET = new THREE.Vector3(0, -25, -10);
+    const LIGHT_POS = new THREE.Vector3(2, 3, 1.25);
+    const CAMERA_POS = new THREE.Vector3(34, 20, 34);
     const UPDATES_PER_SECOND = 24;
 
-    const GRID_WIDTH = 4;
-    const GRID_LENGTH = 4;
+    const GRID_WIDTH = 5;
+    const GRID_LENGTH = 5;
     const GRID_UNIT_LENGTH = 8;
-    const GRID_GUTTER_SIZE = 1;
+    const GRID_GUTTER_SIZE = 0.5;
     // assigned to vars for closures below
     const WINDOW_WIDTH = this.$el.clientWidth;
     const WINDOW_HEIGHT = this.$el.clientHeight;
@@ -36,7 +38,6 @@ export default {
       1000
     );
     camera.position.set (CAMERA_POS.x, CAMERA_POS.y, CAMERA_POS.z);
-    camera.lookAt (CAMERA_TARGET);
 
     let renderer = new THREE.WebGLRenderer ({antialias: true});
     renderer.setSize (this.$el.clientWidth, this.$el.clientHeight);
@@ -53,23 +54,49 @@ export default {
     // Creating boxes
     const boxes = [];
     const gridBoxGroup = new THREE.Group ();
-    const gridBoxGeometry = new THREE.BoxBufferGeometry (GRID_UNIT_LENGTH, GRID_UNIT_LENGTH, GRID_UNIT_LENGTH);
-    const gridBoxMaterial = new THREE.MeshLambertMaterial ({
-      color: 0xdd0000,
-      flatShading: true,
-    });
+    let gridBoxGeometry;
+    let gridBoxMaterial;
+    const lightest = 'dd0000';
+    const darkest = chroma(lightest).darken(4);
+    const colorScale = chroma.scale([darkest, lightest]);
+    const gridUnitWithGutter = GRID_UNIT_LENGTH + GRID_GUTTER_SIZE;
+    let sceneLength = gridUnitWithGutter * GRID_LENGTH;
+    let sceneWidth = gridUnitWithGutter * GRID_WIDTH;
+    let newHeight;
     let newBox;
+
+    console.log(colorScale(0).hex(), colorScale(0.5), colorScale(1))
+    
     for (let i = 0; i < GRID_LENGTH; i++) {
+      boxes[i] = [];
+
       for (let j = 0; j < GRID_WIDTH; j++) {
+        newHeight = utils.getRandomInt(4, GRID_UNIT_LENGTH);
+        gridBoxGeometry = new THREE.BoxBufferGeometry (
+          GRID_UNIT_LENGTH,
+          newHeight,
+          GRID_UNIT_LENGTH,
+        );
+        gridBoxMaterial = new THREE.MeshLambertMaterial ({
+          color: colorScale(sample([0.2, 0.4, 0.6, 0.8, 1.0])).hex(),
+          flatShading: true,
+        });
         newBox = new THREE.Mesh (gridBoxGeometry, gridBoxMaterial);
-        newBox.position.setX(i * (GRID_UNIT_LENGTH + GRID_GUTTER_SIZE));
-        newBox.position.setZ(j * (GRID_UNIT_LENGTH + GRID_GUTTER_SIZE));
-        boxes.push(newBox);
+        newBox.position.setX(i * gridUnitWithGutter);
+        newBox.position.setY(newHeight / 2);
+        newBox.position.setZ(j * gridUnitWithGutter);
+        boxes[i].push(newBox);
         gridBoxGroup.add(newBox);
       }
     }
     scene.add (gridBoxGroup);
-
+    console.log(sceneLength, sceneWidth);
+    const newCameraTarget = new THREE.Vector3(
+      sceneLength / 2,
+      0,
+      sceneWidth / 2
+    );
+    camera.lookAt(newCameraTarget);
 
     ///////////////////////////////////////////////////////////////////////////////
     //   MAIN RENDER/UPDATE LOOPS
