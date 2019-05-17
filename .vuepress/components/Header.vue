5cc92a5642b3332520253a1f7f28124780f8aef0
RJ Salvador <rjsalvadorr@gmail.com>
2019-05-16 20:32:03 -0400

    #9 - Header rework (#16)
    * #7 - Fixed background image issue on mobile menu
    
    * Reworked mobile header functionality for category pages
    
    * #9 - made good progress on the home page
    
    * #9 - Reworked header on homepage

--- .vuepress/components/Header.vue
+++ .vuepress/components/Header.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="page-header">
-    <div class="header-top">
-      <div class="header-wrapper">
-        <a href="/"><h1 class="page-title">{{ title }}</h1></a>
-        <span class="page-desc">{{ desc }}</span>
+  <div :class="getHeaderClass()">
+    <div v-if="!onHomePage">
+      <div class="header-top">
+        <div class="header-wrapper">
+          <a href="/"><h1 class="page-title">{{ title }}</h1></a>
+          <span class="page-desc">{{ desc }}</span>
+        </div>
+        <NavMenu class="nav-wrapper nav-wrapper--desktop" :navigationLinks="$site.themeConfig.nav"></NavMenu>
+        <div class="btn-mobile-burger" v-on:click="toggleMobileMenu">
+          <BurgerIcon :class="getButtonClass()" />
+        </div>
       </div>
-      <NavMenu class="nav-wrapper nav-wrapper--desktop" :navigationLinks="$site.themeConfig.nav"></NavMenu>
-      <div class="btn-mobile-burger" v-on:click="toggleMobileMenu">
-        <BurgerIcon :class="getButtonClass()" />
+      <div class="header--mobile" v-if="mobileMenuOpen">
+        <NavMenu class="nav-wrapper nav-wrapper--mobile" :navigationLinks="$site.themeConfig.nav"></NavMenu>
       </div>
     </div>
-    <div class="header--mobile" v-if="mobileMenuOpen">
-      <NavMenu class="nav-wrapper nav-wrapper--mobile" :navigationLinks="$site.themeConfig.nav"></NavMenu>
+    <div v-else>
+      <div class="header-top header-top--home">
+        <NavMenu class="nav-wrapper nav-wrapper--mobile" :navigationLinks="$site.themeConfig.nav"></NavMenu>
+      </div>
     </div>
   </div>
 </template>
@@ -22,16 +29,31 @@ import BurgerIcon from "./BurgerIcon.vue";
 
 export default {
   name: 'Header',
+  data: function () {
+    return {
+      mobileMenuOpen: false,
+    }
+  },
   props: {
     title: String,
     desc: String,
-    mobileMenuOpen: false,
+    onHomePage: Boolean,
   },
   components: {
     NavMenu,
     BurgerIcon,
   },
   methods: {
+    getHeaderClass() {
+      let headerClass = 'page-header';
+      if(this.mobileMenuOpen) {
+        headerClass += ' page-header--mobile-expanded';
+      }
+      if(this.onHomePage) {
+        headerClass += ' page-header--home';
+      }
+      return headerClass;
+    },
     toggleMobileMenu() {
       this.mobileMenuOpen = !this.mobileMenuOpen;
       console.log(this.mobileMenuOpen);
@@ -56,25 +78,75 @@ export default {
   $burger-height-mobile: 30px;
 
   .page-header {
-    background-color: $header-bg-color;
-    background-image: url("/textures/shley-tree-1.png");
-
     color: $header-color;
-    padding: $space-unit;
     position: relative;
     z-index: $z-index-header;
     box-shadow: $box-shadow-down;
+    height: $header-height-mob;
 
-    .header--mobile {
-      display: block;
+    &--mobile-expanded .header-top {
+      height: $header-height-mob-exp;
+    }
+
+    &--home {
+      height: $header-height-mob-home;
+    }
+
+    .header-top {
+      z-index: $z-index-header - 2;
+      display: flex;
+
+      padding: $space-unit;
       background-color: $header-bg-color;
       background-image: url("/textures/shley-tree-1.png");
       position: absolute;
-      z-index: $z-index-header - 2;
+      left: 0;
+      right: 0;
+
+      &--home {
+        .nav-wrapper.nav-wrapper--mobile {
+          margin-top: 0;
+        }
+      }
+
+      .page-title {
+        margin-top: 0;
+        letter-spacing: 0.1rem;
+        display: inline-block;
+        margin-bottom: 0;
+        font-size: $title-height;
+        line-height: $title-height;
+        color: $header-color;
+
+        &:hover {
+          color: $header-color-hover;
+        }
+      }
+
+      .page-desc {
+        @include subtitle-text;
+        display: block;
+        font-weight: 300;
+        letter-spacing: 0.12rem;
+      }
+
+      .header-wrapper {
+        flex: 1 1 50%;
+      }
+
+      .nav-wrapper {
+        flex: 2 1 50%;
+        margin-top: $space-unit;
+      }
+    }
+
+    .header--mobile {
+      display: block;
+      position: absolute;
+      z-index: $z-index-header - 1;
       left: 0;
       right: 0;
       bottom: -40px;
-      padding-top: 91px;
       padding-bottom: $space-unit * 0.75;
       box-shadow: $box-shadow-down;
     }
@@ -95,42 +167,6 @@ export default {
         opacity: 0.5;
         filter: none;
       }
-    }
-  }
-
-  .header-top {
-    position: relative;
-    z-index: $z-index-header - 1;
-    display: flex;
-
-    .page-title {
-      margin-top: 0;
-      letter-spacing: 0.1rem;
-      display: inline-block;
-      margin-bottom: 0;
-      font-size: $title-height;
-      line-height: $title-height;
-      color: $header-color;
-
-      &:hover {
-        color: $header-color-hover;
-      }
-    }
-
-    .page-desc {
-      @include subtitle-text;
-      display: block;
-      font-weight: 300;
-      letter-spacing: 0.12rem;
-    }
-
-    .header-wrapper {
-      flex: 1 1 50%;
-    }
-
-    .nav-wrapper {
-      flex: 2 1 50%;
-      margin-top: $space-unit;
     }
   }
 
