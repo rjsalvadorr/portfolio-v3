d2b0282d401c479a4234b9d57030defe0244b4c9
RJ Salvador <rjsalvadorr@gmail.com>
2019-05-17 13:57:07 -0400

    #13 - added contact info on footer

--- .vuepress/components/Footer.vue
+++ .vuepress/components/Footer.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="page-footer">
+    <span class="contact contact-email">
+      <img class="contact-icon" src="/icons/mail.svg" />
+      randolph.salvador@gmail.com
+    </span>
+    <span class="contact contact-linkedin">
+      <img class="contact-icon" src="/icons/linkedin.svg" />
+      linkedin.com/in/randolphsalvador
+    </span>
+    <span class="contact contact-github">
+      <img class="contact-icon" src="/icons/github.svg" />
+      github.com/rjsalvadorr
+    </span>
     <span class="copyright">© {{ getCurrentYear }} Randolph Jerome Salvador</span>
   </div>
 </template>
@@ -26,9 +38,27 @@ export default {
     padding: ($space-unit * 0.75) $space-unit;
     box-shadow: $box-shadow-up;
 
+    .contact {
+      // @include subtitle-text;
+      letter-spacing: 0.05rem;
+      font-size: $body-font-size * 0.8;
+      line-height: $body-font-size * 1.2;
+      display: block;
+
+      &-icon {
+        display: inline-block;
+        height: $body-font-size * 0.95;
+        width: $body-font-size * 0.95;
+        margin-right: $body-font-size / 4;
+        vertical-align: middle;
+      }
+    }
+
     .copyright {
       @include subtitle-text;
       letter-spacing: 0.09rem;
+      display: block;
+      margin-top: $space-unit * 0.75;
     }
   }
 </style>
